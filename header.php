1db1313172b13fd66ef119c8a94cb6aa2e6be11f
maralfati <fatimahabeb855@gmail.com>
2023-03-16 02:36:35 -0400

    upload new

---
+++ header.php
@@ -0,0 +1,17 @@
+<?php include("myDatas.php");?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>my test</title>
+</head>
+<body>
+    <div>
+        <!-- يوجد مشكلة لم افهم حلها -->
+        <a href="<?php echo SITEURL;?>add.php">press here</a>
+    </div>
+    
+</body>
+</html>
